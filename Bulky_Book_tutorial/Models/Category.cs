b73a24ed6d9fb9d37353956e2b491aa80f5a6952
Yura <urakost427@gmail.com>
2023-03-29 20:37:26 +0300

    Created Category model

---
+++ Bulky_Book_tutorial/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulky_Book_tutorial.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreationDateTime { get; set; } = DateTime.Now;
+
+    }
+}
