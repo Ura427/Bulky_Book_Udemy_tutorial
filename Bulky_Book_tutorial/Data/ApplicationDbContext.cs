9fa4b40a5d59957fbb534ee34b6d33559fc942a4
Yura <urakost427@gmail.com>
2023-03-29 20:37:07 +0300

    Created Database context

---
+++ Bulky_Book_tutorial/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bulky_Book_tutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky_Book_tutorial.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
