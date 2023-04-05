d8920037a3d6111b1417e1276842341bbbcf8850
Yura <urakost427@gmail.com>
2023-04-05 23:32:33 +0300

    Created repository interface for CoverType table

---
+++ BulkyBook.DataAccess/Repository/IRepository/ICoverTypeRepository .cs
@@ -0,0 +1,10 @@
+﻿using BulkyBook.Models;
+using BulkyBook.Repository.IRepository;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType obj);
+    }
+}
