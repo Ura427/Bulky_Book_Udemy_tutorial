1d81ea638781eb9b8eddf7d421e2d4e855a3b08f
Yura <urakost427@gmail.com>
2023-04-05 21:08:21 +0300

    Created IRepository interface

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace BulkyBook.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //T - Category
+        IEnumerable<T> GetAll();
+
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+
+    }
+}
