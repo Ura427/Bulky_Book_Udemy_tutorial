346b4a7647167eb159f799c2551383055415a14c
Yura <urakost427@gmail.com>
2023-04-05 23:40:09 +0300

    Created Covertype model

---
+++ BulkyBook.Models/CoverType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+   public class CoverType
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Cover name")]
+        public string Name { get; set; }
+    }
+}
