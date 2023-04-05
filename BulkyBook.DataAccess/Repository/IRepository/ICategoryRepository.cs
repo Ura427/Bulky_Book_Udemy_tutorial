efb4444434ff5a2143086c02822c7d9fb2012ed7
Yura <urakost427@gmail.com>
2023-04-05 23:31:57 +0300

    Removed Save method

--- BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -6,6 +6,5 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     public interface ICategoryRepository : IRepository<Category>
     {
         void Update(Category obj);
-        void Save();
     }
 }
