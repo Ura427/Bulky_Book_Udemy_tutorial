fc3b7fc638fdc8bbab86cd3faf08d63147720ff1
Yura <urakost427@gmail.com>
2023-04-07 12:39:42 +0300

    Created ProductRepository class

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Repository;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(Product obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
