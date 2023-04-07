2317b1809439670c6b494c73bc0be603458a4012
Yura <urakost427@gmail.com>
2023-04-07 12:40:19 +0300

    Added  IProductRepository property and reference to Products table in Db

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -7,6 +7,7 @@ namespace BulkyBook.DataAccess.Repository
     {
         public ICategoryRepository Category { get; private set; }
         public ICoverTypeRepository CoverType { get; private set; }
+        public IProductRepository Product { get; private set; }
 
         private readonly ApplicationDbContext _db;
 
@@ -15,6 +16,7 @@ namespace BulkyBook.DataAccess.Repository
             _db = db;
             Category = new CategoryRepository(_db);
             CoverType = new CoverTypeRepository(_db);
+            Product = new ProductRepository(_db);
         }
 
         public void Save()
